c3778330a623d1d26b76b427c1c3bd33605c92e3
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-18 21:36:49 +0300

    Create Dish model at kitchen/models.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class DishType(models.Model):
     name = models.CharField(max_length=63)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=63)
+    description = models.TextField(blank=True)
+    price = models.FloatField()
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
