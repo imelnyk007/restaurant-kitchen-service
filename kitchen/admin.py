70f01637a9bcafd6bb61ef27f900b8dcb2e40e3f
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-18 22:20:42 +0300

    Create class DishAdmin for display fields: name, dish_type, price in the admin page

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -5,6 +5,11 @@ from kitchen.models import DishType, Cook, Dish
 
 admin.site.unregister(Group)
 
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "dish_type", "price"]
+
+
 admin.site.register(DishType)
 admin.site.register(Cook)
-admin.site.register(Dish)
