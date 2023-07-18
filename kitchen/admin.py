dbc7cf37d70fc2427648325b89710b583519bf84
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-18 22:29:57 +0300

    Create class CookAdmin for display field: year_of_experience in the admin page

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
 from kitchen.models import DishType, Cook, Dish
 
 admin.site.unregister(Group)
 
+admin.site.register(DishType)
 
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
     list_display = ["name", "dish_type", "price"]
 
 
-admin.site.register(DishType)
-admin.site.register(Cook)
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
