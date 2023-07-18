a33e8ba9bdedf264134ea9bfde6cec8b3b4b1a51
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-18 21:47:41 +0300

    Add field cook to model Dish

--- kitchen/models.py
+++ kitchen/models.py
@@ -6,12 +6,13 @@ class DishType(models.Model):
     name = models.CharField(max_length=63)
 
 
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+
 class Dish(models.Model):
     name = models.CharField(max_length=63)
     description = models.TextField(blank=True)
     price = models.FloatField()
     dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
-
-
-class Cook(AbstractUser):
-    years_of_experience = models.IntegerField()
+    cooks = models.ManyToManyField(Cook, related_name="cooks")
